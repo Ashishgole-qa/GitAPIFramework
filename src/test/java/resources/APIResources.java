e1baff13af4e07ddbb6de7daedd748bbb1b4f579
Ashish <Ashishgole0@gmail.com>
2020-05-16 12:06:35 +0530

    "Cpitalization of API names"

--- src/test/java/resources/APIResources.java
+++ src/test/java/resources/APIResources.java
@@ -3,8 +3,8 @@ package resources;
 public enum APIResources {
 	
 	AddPlaceAPI("/maps/api/place/add/json"),
-	getPlaceAPI("/maps/api/place/get/json"),
-	deletePlaceAPI("/maps/api/place/delete/json");
+	GetPlaceAPI("/maps/api/place/get/json"),
+	DeletePlaceAPI("/maps/api/place/delete/json");
 	private String resource;
 	
 	APIResources(String resource)
